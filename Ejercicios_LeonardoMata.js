0d9dd94367641b4ead2aeab71454e184a45dc294
yolo1 <leonardo.mata@institutvidreres.cat>
2024-11-27 23:12:39 +0100

    5 y 6

--- Ejercicios_LeonardoMata.js
+++ Ejercicios_LeonardoMata.js
@@ -1,3 +1,4 @@
+// Ejercicio 1
 console.log("=====Ejercicio 1=====")
 
 let numeros = Array.from({length: 5}, () => Math.floor(Math.random() * 100));
@@ -11,11 +12,13 @@ console.log("Array ordenao:", numeros);
 console.log("Primer Valor:", valorprimero);
 console.log("Ultimo Valor:", valorultimo);
 
+// Ejercicio 2
 console.log("=====Ejercicio 2=====")
 
 let otroarray = [...numeros];
 console.log("Nuevo Array usando operador de propagación", otroarray);
 
+// Ejercicio 3
 console.log("=====Ejercicio 3=====")
 
 let listanum = [10,20,30, ...numeros];
@@ -26,6 +29,7 @@ listanum.forEach(num => {
 });
 console.log("Array fusionado sin duplicados", fusion);
 
+// Ejercicio 4
 console.log("=====Ejercicio 4=====");
 
 let noduplica = new Map();
@@ -34,3 +38,21 @@ let resultadoDuplicados = Array.from(noduplica.keys());
 
 console.log("Lista sin duplicados:", resultadoDuplicados);
 
+// Ejercicio 5
+console.log("=====Ejercicio 5=====");
+
+function cubos(array) {
+    return array.map(num => num ** 3);
+}
+let arrayCubos = cubos([1, 3, 6]);
+console.log("Cubos array: ", arrayCubos);
+
+// Ejercicio 6
+console.log("=====Ejercicio 6=====");
+
+let nombresConA = ["ahiram", "andres", "amargo", "erica", "vallcanera"];
+let stringConMuchaA = nombresConA.filter(str => {
+    let contadorA = str.split('').filter(char => char === 'a').length;
+    return contadorA > 1;
+});
+console.log("Strings con mas de una A: ", stringConMuchaA);
